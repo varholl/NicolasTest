12bf629563259349ac3e8ce51ae55591d6bf9bc6
Matias Laino <matias.laino@passare.com>
2022-10-27 16:00:29 -0300

    Fixed specs

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -23,8 +23,8 @@ RSpec.describe Player, type: :model do
     end
 
     describe 'validators' do
-      it {should validate_numericality_of(:height).is_greater_than(-1)}
-      it {should validate_numericality_of(:weight).is_greater_than(-1)}
+      it {should validate_numericality_of(:height).is_greater_than(0)}
+      it {should validate_numericality_of(:weight).is_greater_than(0)}
       it {should validate_presence_of(:first_name)}
       it {should validate_presence_of(:last_name)}
     end
